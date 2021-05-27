721fbabc2718bc5046141660f4504ebe43b5a307
Lyubov222 <lyubov.pan2000@mail.ru>
2021-05-27 21:20:41 +0700

    создан класс Менеджер проекта

--- ContactsAppUI/ContactsApp/ProjectManager.cs
+++ ContactsAppUI/ContactsApp/ProjectManager.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
 
 namespace ContactsApp
 {
-    class ProjectManager
+    /// <summary>
+    /// Класс сериализации
+    /// </summary>
+    public static class ProjectManager
     {
+        /// <summary>
+        /// Serializer
+        /// </summary>
+        private static readonly JsonSerializer _serializer = new JsonSerializer();
+
+        
+
     }
 }
