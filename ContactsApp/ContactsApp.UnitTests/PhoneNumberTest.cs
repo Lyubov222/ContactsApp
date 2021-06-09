5bc0b1734f2c1b4884e7cb891ef2924ccec9acfe
Lyubov222 <lyubov.pan2000@mail.ru>
2021-06-09 22:00:16 +0700

    Прописан юнит тест для PhoneNumber

--- ContactsApp/ContactsApp.UnitTests/PhoneNumberTest.cs
+++ ContactsApp/ContactsApp.UnitTests/PhoneNumberTest.cs
@@ -1,13 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using NUnit.Framework;
 
 namespace ContactsApp.UnitTests
 {
     /// <summary>
     /// Класс тестов для тестирования PhoneNumber
-    /// </summary>
-    class PhoneNumberTest
+    /// </summary> 
+    public class PhoneNumberTest
     {
+        private PhoneNumber _testNumber;
+
+        public void SetUp()
+        {
+            _testNumber = new PhoneNumber();
+        }
+
+        [Test(Description = "Позитивный тест геттера Number")]
+        public void TestNumberGet_CorrectValue()
+        {
+            SetUp();
+
+            var expected = 79531817562;
+            _testNumber.Number = expected;
+            var actual = _testNumber.Number;
+            Assert.AreEqual(expected, actual, "Геттер Number возвращает неправильный номер");
+        }
+
+        [Test(Description = "Позитивный тест сеттера Number")]
+        public void TestNumberSet_CorrectValue()
+        {
+            SetUp();
+
+            _testNumber.Number = 79523658444;
+            var actual = _testNumber.Number;
+            Assert.AreEqual(actual, _testNumber.Number, "Сеттер неправильно заполнил номер");
+        }
+
+        [TestCase(null, "Должно возникать исключение, если телефон - пустая строка",
+            TestName = "Присвоение пустой строки в качестве номера")]
+        [TestCase(789, "Должно возникать исключение, если номер короче 11 символов",
+            TestName = "Присвоение неправильного номера короче 11 символов")]
+        [TestCase(789878978788, "Должно возникать исключение, если номер длиннее 11 символов",
+            TestName = "Присвоение неправильного номера длиннее 11 символов")]
+        public void TestNumberSet_ArgumentException(long wrongNumber, string message)
+        {
+            SetUp();
+
+            Assert.Throws<ArgumentException>(
+                () => { _testNumber.Number = wrongNumber; }, message);
+        }
     }
 }
